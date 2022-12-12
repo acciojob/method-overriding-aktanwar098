c6677a317114dff5592f99f51f484b525605c5dd
aktanwar098 <ak.tanwar098@gmail.com>
2022-12-12 20:39:54 +0530

    updating

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,12 +12,12 @@ public class Main {
         @Override
         public String meth(){
 
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
     }
 
     public static void main(String[] args) {
-        B b = new B();
-        System.out.println(b.meth());
+        B obj = new B();
+        obj.meth();
     }
 }
